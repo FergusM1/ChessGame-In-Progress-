b37e4fc9ef07601863c2d4a52343df3ea0affcc3
Fergus McCune <mccunefergus@gmail.com>
2025-02-21 15:42:37 +0200

    Uploading unfinished code

---
+++ ChessGame/ChessLogic/Pieces/Pawn.cs
@@ -0,0 +1,22 @@
+﻿namespace ChessLogic
+{
+    public class Pawn : Piece
+    {
+        public override PieceType Type => PieceType.Pawn;
+
+        public override Player Colour { get; }
+
+        public Pawn(Player colour)
+        {
+            Colour = colour;
+        }
+
+        public override Piece Copy()
+        {
+            Pawn copy = new Pawn(Colour);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+
+    }
+}
---
+++ ChessGame/ChessUI/Images.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ChessLogic;
+
+namespace ChessUI
+{
+    public static class Images
+    {
+        private static readonly Dictionary<PieceType, ImageSource> whiteSources = new()
+        {
+            {PieceType.Pawn, LoadImage("ChessAssets/PawnW.png")},
+            {PieceType.Bishop, LoadImage("ChessAssets/BishopW.png")},
+            {PieceType.Knight, LoadImage("ChessAssets/KnightW.png")},
+            {PieceType.Rook, LoadImage("ChessAssets/RookW.png")},
+            {PieceType.Queen, LoadImage("ChessAssets/QueenW.png")},
+            {PieceType.King, LoadImage("ChessAssets/KingW.png")}
+
+        };
+
+        private static readonly Dictionary<PieceType, ImageSource> blackSources = new()
+        {
+            {PieceType.Pawn, LoadImage("ChessAssets/PawnB.png")},
+            {PieceType.Bishop, LoadImage("ChessAssets/BishopB.png")},
+            {PieceType.Knight, LoadImage("ChessAssets/KnightB.png")},
+            {PieceType.Rook, LoadImage("ChessAssets/RookB.png")},
+            {PieceType.Queen, LoadImage("ChessAssets/QueenB.png")},
+            {PieceType.King, LoadImage("ChessAssets/KingB.png")}
+
+        };
+        private static ImageSource LoadImage(string filePath)
+        {
+            return new BitmapImage(new Uri(filePath, UriKind.Relative));
+        }
+
+        public static ImageSource GetImage(Player colour, PieceType type)
+        {
+            return colour switch
+            {
+                Player.White => whiteSources[type],
+                Player.Black => blackSources[type],
+                _ => null
+            };
+        }
+
+        public static ImageSource GetImage(Piece piece)
+        {
+            if (piece == null)
+            {
+                return null;
+            }
+
+            return GetImage(piece.Colour, piece.Type);
+        }
+        
+   
+    }
+}
